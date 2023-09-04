ef340be9f2e36063db8044083ab8595660c41acf
Naser <naser_shm@yahoo>
2023-09-04 21:19:32 +0330

    ShopManagement

---
+++ 0-FrameWork/Domain/EntityBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_FrameWork.Domain
+{
+    public class EntityBase
+    {
+        public long Id { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public DateTime CreationDate { get;private set; }
+
+        public EntityBase()
+        {
+            IsDeleted = false;
+            CreationDate = DateTime.Now;
+        }
+    }
+}
---
+++ SM.Domain/ProductAgg/Product.cs
@@ -0,0 +1,14 @@
+﻿using _0_FrameWork.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.Domain.ProductAgg
+{
+   public class Product : EntityBase
+    {
+        
+    }
+}
